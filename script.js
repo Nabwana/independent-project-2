b8063387408c35343f883050b605ec16d72af595
pascal <pnabwana13@gmail.com>
2019-02-15 13:43:12 +0300

    add script to webpage

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function Results(){
-  var hypotenuse = parseInt(prompt("Enter side digits here"));
-  var adjacent = parseInt(prompt("Enter side digits here"));
-  var opposite = parseInt(prompt("Enter side digits here"));
-}
-function Results(){
-  var side =[]
+function results () {
 
+var hypotenuse =parseInt(document.getElementById("hypotenuse").value);
+var adjacent =parseInt(document.getElementById("opposite").value);
+var opposite =parseInt(document.getElementById("adjacent").value);
+
+
+
+else if( (hypotenuse+adjacent)<=opposite||(hypotenuse+opposite)<=adjacent||(adjacent+opposite)<=hypotenuse ){
+    alert("This is not a Triangle");
+}
+
+else if (hypotenuse===adjacent && hypotenuse===opposite && adjacent===opposite){
+ alert("This is an Equilateral Triangle");
+}
+
+else if (hypotenuse===adjacent||hypotenuse===opposite||adjacent===opposite){
+    alert("This is an Isosceles");
+}
+
+else if (hypotenuse!==adjacent||hypotenuse!==opposite&&adjacent!==opposite){
+    alert("This is a scalene");
+  }
 }
