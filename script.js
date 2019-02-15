de0c1942089554a6605a3ae76c92d8693246800c
pascal <pnabwana13@gmail.com>
2019-02-15 11:47:09 +0300

    added javascript instructions

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function Results(){
-  
+  var hypotenuse = parseInt(prompt("Enter side digits here"));
+  var adjacent = parseInt(prompt("Enter side digits here"));
+  var opposite = parseInt(prompt("Enter side digits here"));
+}
+function Results(){
+  var side =[]
+
 }
