b76d54fcd2982824a11a769cf36b31c9ffcdb88b
pascal <pnabwana13@gmail.com>
2019-02-15 14:36:44 +0300

    add modified script to web page

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ var adjacent =parseInt(document.getElementById("opposite").value);
 var opposite =parseInt(document.getElementById("adjacent").value);
 
 
+if (hypotenuse===adjacent && hypotenuse===opposite && adjacent===opposite){
+ alert("This is an Equilateral Triangle");
+}
 
 else if( (hypotenuse+adjacent)<=opposite||(hypotenuse+opposite)<=adjacent||(adjacent+opposite)<=hypotenuse ){
     alert("This is not a Triangle");
 }
 
-else if (hypotenuse===adjacent && hypotenuse===opposite && adjacent===opposite){
- alert("This is an Equilateral Triangle");
-}
 
 else if (hypotenuse===adjacent||hypotenuse===opposite||adjacent===opposite){
     alert("This is an Isosceles");
